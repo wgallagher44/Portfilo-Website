1e4a5e10b62a9469a49da696557494f217b129d8
Will Gallagher <100447981+wgallagher44@users.noreply.github.com>
2022-07-14 23:41:19 -0400

    Fourth Commit

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,11 +1,27 @@
-import React from "react";
+
 import gitImage from "../Images/github_logo.png"
 import linkedIn from "../Images/linkedIn_icon.png"
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 
 function Email (){
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_jox4d45', 'template_aiqf1iu', form.current, 'LF4pDJ8XbTOG0mpjT')
+        .then((result) => {
+            alert("Message Has been sent to Will")
+            console.log(result.text);
+            console.log("message sent")
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    };
     return (
         <section id = "Contact Me"className="email">
            <h3 className="email-heading">Contact Me</h3> 
@@ -15,15 +31,21 @@ function Email (){
 
            </div>
            <div className="email-contact">willgallagher0702@gmail.com/ 717-209-1084</div> 
-           <div className="email-section">
+           <form ref ={form} onSubmit = {sendEmail}>
+            <div className="email-section">
+
             <div className="email-info">
-                  <input type="text" className="email-name" placeholder="Your Name"></input>
-            <input type="text" className="email-subject" placeholder="Subject"></input>
+            <input type="text" className="email-name" placeholder="Your Name" name="name" id = "name"></input>
+            <input type="text" className="email-subject" placeholder="Subject" name="subject" id = "subject"></input>
             </div>
-            <textarea type="text" className="email-message" placeholder="Message"></textarea>
+            <textarea type="text" className="email-message" placeholder="Message" name="message" id = "message"></textarea>
            </div>
             
-          <button className="email-send">Send</button>
+          <input type="submit" className="email-send" value="Send" />
+
+
+           </form>
+           
 
         </section>
     );
